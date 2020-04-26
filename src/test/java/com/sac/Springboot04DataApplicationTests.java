9d071273f068ec929afdde9e1cc967932890f101
SAC <392063541@qq.com>
2020-04-26 14:50:03 +0800

    SpringBoot整合数据库

---
+++ src/test/java/com/sac/Springboot04DataApplicationTests.java
@@ -0,0 +1,32 @@
+package com.sac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootTest
+class Springboot04DataApplicationTests {
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void contextLoads() throws SQLException {
+        //查看默认的数据源
+        System.out.println(dataSource.getClass());
+
+        //获取数据库连接
+        Connection conn = dataSource.getConnection();
+        System.out.println(conn);
+
+        //xxx Template:springBoot已经配置好模板bean，拿来即用
+
+
+
+        conn.close();
+    }
+
+}
